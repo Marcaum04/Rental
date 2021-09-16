2c70a724e516cabd0a9eaa978a0ac9d15e92ed30
Marcos <mvinicius.oliveira04@gmail.com>
2021-09-16 19:53:06 -0300

    Commit inicial

---
+++ API/Senai.Rental.WebApi/Senai.Rental.WebApi/Domains/VeiculoDomain.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Senai.Rental.WebApi.Domains
+{
+    public class VeiculoDomain
+    {
+        public int idVeiculo { get; set; }
+        public EmpresaDomain empresa { get; set; }
+        [Required(ErrorMessage = "Todo carro tem um cor")]
+        public string cor { get; set; }
+        public string placa { get; set; }
+    }
+}
